7c473c9ddd4d139ae569279e772353102847899e
hungnd2905 <hungnd2905@gmail.com>
2022-10-04 05:08:30 +0200

    add login api endpoint

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,10 @@ namespace API.DTOs
     public class RegisterDto
     {
         public int Id {get; set;}
+
+        [Required]
         public string UserName {get; set;}
+        [Required]
         public string Password {get; set;}
     }
 }
