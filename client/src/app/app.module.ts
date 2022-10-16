60491d04dd8afee963cfb8e426772d292302a651
hungnd2905 <hungnd2905@gmail.com>
2022-10-17 00:14:23 +0200

    add server error and exception handling middleware

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -13,7 +13,7 @@ import { MemberListComponent } from './members/member-list/member-list.component
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
-import { ToastrModule } from 'ngx-toastr';
+import { SharedModule } from './_modules/shared.module';
 
 @NgModule({
   declarations: [
@@ -32,9 +32,7 @@ import { ToastrModule } from 'ngx-toastr';
     HttpClientModule,
     NgbModule,
     FormsModule,
-    ToastrModule.forRoot({
-      positionClass: 'toast-bottom-right',
-    }),
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
